d260994dcaf1e8e136667555cec52c47e70754c9
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 18:30:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,4 +50,10 @@ attr_accessor :id, :name, :grade
     grade = row[2]
     self.new(id,name,grade)
   end
+
+  def self.find_by_name(name)
+    sql = "SELECT FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql,name)[0]
+    Student.new(result[0], result[1], result [2])
+  end
 end
