dde909276e4000cb9d04e8bcc6e97f8333bb8691
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 18:07:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,15 +25,15 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
 
-  def update 
+  def update
     sql= "UPDATE students SET name = ?, grade = ? WHERE id = ?;"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end 
+  end
 
   def save
-    if self.id 
-      self.update 
-    else 
+    if self.id
+      self.update
+    else
     sql = "INSERT INTO students (name, grade)
     VALUES (?,?);"
 
